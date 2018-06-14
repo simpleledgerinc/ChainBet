c385fa2dafd0eb1a390b6d0f3ac0470fd5372851
James Cramer <jcramer@deskviz.com>
2018-06-14 16:38:06 -0400

    byte chunk workaround / hotfix

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 ## Usage
 
 ```js
-let cb = require('chainbet');
-let chainbet = new cb()
-chainbet.encodePhase1('01', 12345, 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c')
+let chainbet = require('chainbet');
+chainbet.encodePhase1(0x01, 1000, 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c')
 // <Buffer 6a 04 54 45 42 00 02 01 00 02 01 00 02 01 00 02 01 00 05 31 32 33 34 35 36 62 69 74 63 6f 69 6e 63 61 73 68 3a 71 7a 73 30 32 76 30 35 6c 37 71 73 35 ... >
 
 let asm = 'OP_RETURN 00424554 01010100000031323334353f8b68135f399b101868b540decc00207906f6af3'
--- index.js
+++ index.js
@@ -28,16 +28,24 @@ module.exports = class Chainbet {
       0x01,
       // 1 byte bet type id
       type,
-      // 8 byte amount
-      this.amount2Hex(amount)
     ];
+    
+    // add 8 byte amount
+    amount = this.amount2Hex(amount)
+    amount.forEach((item, index) => {
+      script.push(item);
+    })
 
+    // add optional 20 byte target address
     if(targetAddress != undefined) {
       // optional 20 byte HASH160 public key hash
-      script.push(BITBOX.Crypto.hash160(targetAddress));
+      let addr = BITBOX.Crypto.hash160(targetAddress);
+      addr.forEach((item, index) => { script.push(item); })
     }
 
-    return BITBOX.Script.encode(script);
+    let encoded = BITBOX.Script.encode(script);
+    //let asm = BITBOX.Script.toASM(encoded);
+    return encoded;
   } 
 
   // Phase 2: Bet Participant Acceptance
@@ -61,11 +69,16 @@ module.exports = class Chainbet {
       0x01,
       // 1 byte phase id
       0x02,
-      // 32 byte betTxId hex
-      betTxId,
-      // 33 byte participant (Bob) multisig Pub Key hex 
-      multisigPubKey,
     ];
+
+    // 32 byte betTxId hex
+    betTxId = Buffer(betTxId, 'hex')
+    betTxId.forEach((item, index) => { script.push(item); })
+
+    // 33 byte participant (Bob) multisig Pub Key hex 
+    multisigPubKey = Buffer(multisigPubKey, 'hex')
+    multisigPubKey.forEach((item, index) => { script.push(item); })
+
     return BITBOX.Script.encode(script)
   }
 
@@ -90,15 +103,24 @@ module.exports = class Chainbet {
       0x01,
       // 1 byte phase id
       0x03,
-      // 32 byte bet tx id
-      betTxId,
-      // 32 byte participant tx id
-      participantTxId,
-      // 32 byte host P2SH id
-      hostP2SHTxId,
-      // 33 byte host (Alice) Multisig Pub Key
-      hostMultisigPubKey,
     ];
+
+    // 32 byte bet tx id
+    betTxId = Buffer(betTxId, 'hex')
+    betTxId.forEach((item, index) => { script.push(item); })
+
+    // 32 byte participant tx id
+    participantTxId = Buffer(participantTxId, 'hex')
+    participantTxId.forEach((item, index) => { script.push(item); })
+
+    // 32 byte host P2SH id
+    hostP2SHTxId = Buffer(hostP2SHTxId, 'hex')
+    hostP2SHTxId.forEach((item, index) => { script.push(item); })
+
+    // 33 byte host (Alice) Multisig Pub Key
+    hostMultisigPubKey = Buffer(hostMultisigPubKey, 'hex')
+    hostMultisigPubKey.forEach((item, index) => { script.push(item); })
+
     return BITBOX.Script.encode(script)
   }
 
@@ -123,15 +145,24 @@ module.exports = class Chainbet {
         0x01,
         // 1 byte phase id
         0x04,
-        // 32 byte bet tx id
-        betTxId,
-        // 32 byte Participant tx id
-        participantTxId,
-        // 72 byte Participant signature 1
-        participantSig1,  // TODO: check for padding (71 vs 72 bytes)
-        // 72 byte Participant signature 2
-        participantSig2,  // TODO: check for padding (71 vs 72 bytes)
       ];
+
+      // 32 byte bet tx id
+      betTxId = Buffer(betTxId, 'hex')
+      betTxId.forEach((item, index) => { script.push(item); })
+
+      // 32 byte Participant tx id
+      participantTxId = Buffer(participantTxId, 'hex')
+      participantTxId.forEach((item, index) => { script.push(item); })
+
+      // 72 byte Participant signature 1
+      participantSig1 = Buffer(participantSig1, 'hex')  // TODO: check for padding (71 vs 72 bytes)
+      participantSig1.forEach((item, index) => { script.push(item); })
+
+      // 72 byte Participant signature 2
+      participantSig2 = Buffer(participantSig2, 'hex')  // TODO: check for padding (71 vs 72 bytes)
+      participantSig2.forEach((item, index) => { script.push(item); })
+
       return BITBOX.Script.encode(script)
     }
 
@@ -161,11 +192,16 @@ module.exports = class Chainbet {
         0x01,
         // 1 byte phase id
         0x06,
-        // 32 byte bet txn id
-        betTxId,
-        // 32 byte Secret value
-        secretValue,
       ];
+
+      // 32 byte bet txn id
+      betTxId = Buffer(betTxId, 'hex')
+      betTxId.forEach((item, index) => { script.push(item); })
+
+      // 32 byte Secret value
+      secretValue = Buffer(secretValue, 'hex')
+      secretValue.forEach((item, index) => { script.push(item); })
+
       return BITBOX.Script.encode(script)
     }
 
@@ -176,7 +212,8 @@ module.exports = class Chainbet {
       for (let i = 0; i < 16 - len; i++) {
         hex = '0' + hex;
       }
-      return Buffer.from(hex, 'hex')
+      let buf = Buffer.from(hex, 'hex')
+      return buf
     }
 
     decode(op_return) {
