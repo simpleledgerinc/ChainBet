caaa4080d8ec16165af8bca8d62818063a748784
James Cramer <jcramer@deskviz.com>
2018-07-05 19:22:22 -0400

    improved selection UI

--- examples/coinflip.js
+++ examples/coinflip.js
@@ -6,7 +6,7 @@ var chainbet = require('../chainbet');
 
 coinflip
     .version('0.0.1')
-    .option('-r, --role [role]', 'set role, 1=Host, 2=Client')
+    .option('-r, --role [role]', 'set role')
     .option('-d, --debug [debug]', 'set debugger support (skips user prompts with default values)')
     .option('-n, --newAddress [newAddress]', 'create a new wallet and address')
     .parse(process.argv);
@@ -21,22 +21,29 @@ if(!coinflip.debug) {
         console.log('\n');
         var questions = [];
         if(true)
-            questions.push({ type: "input", 
+            questions.push({ 
+                            type: "list", 
                             name: "role", 
-                            message: "Enter '1' to host a coinflip, OR Enter '2' to join a coinflip:",
-                            validate: function(input){ if(input == "1" || input == "2") {return true; } else { return false; }}});
+                            message: "What do you want to do?",
+                            choices: [{ name: 'Host coinflip bet', value: 'host' }, 
+                                        { name:'Join coinflip bet', value: 'client' }, 
+                                        //{ name: 'Import new private key', value: 'import' }, 
+                                        //{ name: 'Withdraw funds', value: 'withdraw' }
+                                    ],
+                        });
         return questions;
     }())
 
     .then(function(answers) {  // 2) if args were not specified specified, then try to load them from disk
+        console.log(answers.role)
         coinflip.role = answers.role;
         var wallet;
-        if(coinflip.role == "1")
+        if(coinflip.role == 'host')
             wallet = jsonfile.readFileSync('./wallet.json')[0];
-        else if(coinflip.role == "2")
+        else if(coinflip.role == 'client')
             wallet = jsonfile.readFileSync('./wallet.json')[1];
          else 
-            throw new Error("The input value is not either 1 or 2.");
+            throw new Error("Not a valid selection.");
         
         console.log("\nYour address is: " + wallet.address);
         
@@ -70,7 +77,7 @@ async function main(context) {
     // escrows = await chainbet.CoinFlipShared.processOldEscrows(escrows);
 
     // 3) startup a single bet workflow
-    if (context.role == "1") {
+    if (context.role == 'host') {
         // Host Mode
         bet = new chainbet.CoinFlipHost(context.wif, context.pubkey, context.address, chainfeed);
         bet.run();
@@ -104,11 +111,11 @@ async function debug(context){
     var wallet;
 
     // Host mode debug (i.e., with args "-d 1 -r 1")
-    if(context.role == "1")
+    if(context.role == "host")
         wallet = jsonfile.readFileSync('./examples/wallet.json')[0];
 
     // Host mode debug (i.e., with args "-d 1 -r 1")
-    else if(context.role == "2")
+    else if(context.role == "client")
         wallet = jsonfile.readFileSync('./examples/wallet.json')[1];
 
     console.log("\nYour address is: " + wallet.address);
