1d47c2711dc5e1b8a68e04b45467554a9252b75b
James Cramer <jcramer@deskviz.com>
2018-07-08 16:50:45 -0400

    refactored main()

--- examples/dice.js
+++ examples/dice.js
@@ -2,133 +2,108 @@ let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default;
 let BITBOX = new BITBOXCli();
 
 let fs = require('fs');
-let coinflip = require('commander');
+let context = require('commander');
 let inquirer = require('inquirer');
 let jsonfile = require('jsonfile');
 let chainbet = require('../chainbet');
 
-coinflip
+context
     .version('0.0.12')
     .option('-s, --selection [selection]', 'set initial menu selection')
     .option('-d, --debug [debug]', 'set debugger support (skips user prompts with default values)')
     .parse(process.argv);
-coinflip.debug = (coinflip.debug == "1" ? true : false);
-coinflip.cli = true;
+context.debug = (context.debug == "1" ? true : false);
+//context.cli = true;
 
-// 1) save user specified args to disk
-// TODO LATER
+if(!context.debug)
+    main();
+else if(context.debug)
+    throw new Error("not implemented.");
 
-console.log('\n-------------------------------------------------------------------------------');
-console.log('|                        Welcome to Satoshi Dice!                             |');
-console.log('-------------------------------------------------------------------------------');
-console.log('    =====                   .-------.    ______                       =====');
-console.log('     ///                   /   o   /|   /\\     \\                       ///');
-console.log('     ///                  /_______/o|  /o \\  o  \\                      ///');
-console.log('     ///                  | o     | | /   o\\_____\\                     ///');
-console.log('     ///                  |   o   |o/ \\o   /o    /                     ///');
-console.log('     ///                  |     o |/   \\ o/  o  /                      ///');
-console.log("    =====                 '-------'     \\/____o/                      =====");
-console.log('-------------------------------------------------------------------------------');
-console.log("|                      P2P Gaming with Bitcoin Cash                           |");
-console.log('-------------------------------------------------------------------------------\n');
+async function main() {
+    var wallet;
 
-if(!coinflip.debug) {
-    inquirer.prompt(function() { // 1) Ask the user to specify role (Host or Client)
-        var questions = [];
-        if(true)
-            questions.push({ 
-                type: "list", 
-                name: "selection", 
-                message: "What do you want to do?",
-                choices: 
-                [
-                    new inquirer.Separator("Games"),
-                    { name: '  Play Even-Odd as Host (ODD wins)', value: 'host' }, 
-                    { name: '  Play Even-Odd as Client (EVEN wins)', value: 'client' }, 
-                    new inquirer.Separator("Tools"),
-                    { name: '  Generate new address', value: 'generate' }, 
-                    { name: '  Withdraw funds', value: 'withdraw' }
-                ],
-            });
-        return questions;
-    }())
+    // present main menu to user
+    context.selection = await promptMainMenu();
 
-    .then(function(answers) {
-        coinflip.selection = answers.selection;
-        var wallet, wif;
-
-        if(fs.existsSync('./wallet.json')) {
-            switch (answers.selection) {
-                case 'generate':
-                    console.log("\nGenerating a new address...");
-                    wif = chainbet.Utils.getNewPrivKeyWIF();
-                    wallet = jsonfile.readFileSync('./wallet.json');
-                    wallet.push({ 'wif' : wif });
-                    jsonfile.writeFileSync("./wallet.json", wallet, 'utf8');
-                    break;
-                default:
-                    break;
-            }
+    // check if wallet.json file exists
+    if(fs.existsSync('./wallet.json')) {
+        if (context.selection == 'generate') {
+            console.log("\nGenerating a new address...");
+            var wif = chainbet.Utils.getNewPrivKeyWIF();
+            wallet = jsonfile.readFileSync('./wallet.json');
+            wallet.push({ 'wif' : wif });
+            jsonfile.writeFileSync("./wallet.json", wallet, 'utf8');
         }
-        else {
-            console.log("\nGenerating a new address and wallet.json file...");
-            wif = chainbet.Utils.getNewPrivKeyWIF();
-            fs.writeFile('./wallet.json', "", 'utf8');
-            jsonfile.writeFileSync("./wallet.json", [{ 'wif': wif }], 'utf8');
-        }
+    }
+    else if(!fs.existsSync('./wallet.json')) {
+        console.log("\nGenerating a new address and wallet.json file...");
+        var wif = chainbet.Utils.getNewPrivKeyWIF();
+        fs.writeFile('./wallet.json', "", 'utf8');
+        jsonfile.writeFileSync("./wallet.json", [{ 'wif': wif }], 'utf8');
+    }
 
-        wallet = jsonfile.readFileSync('./wallet.json');
-        wif = wallet[wallet.length - 1].wif;
-                
-        if(coinflip.wif == undefined) {
-            coinflip.wif = wif;
-        }
+    wallet = jsonfile.readFileSync('./wallet.json');
+    context.wif = wallet[wallet.length - 1].wif;
+    let ecpair = BITBOX.ECPair.fromWIF(context.wif);
+    context.pubkey = BITBOX.ECPair.toPublicKey(ecpair);
+    context.address = BITBOX.ECPair.toCashAddress(ecpair);
+    console.log("\nYour address is: " + context.address);
 
-        let ecpair = BITBOX.ECPair.fromWIF(coinflip.wif);
-        coinflip.pubkey = BITBOX.ECPair.toPublicKey(ecpair);
-        coinflip.address = BITBOX.ECPair.toCashAddress(ecpair);
-
-        console.log("\nYour address is: " + coinflip.address);
-    })
-
-    .then(function(){
-        main(coinflip);
-    });    
-} else {
-    debug(coinflip);
-}
-
-async function main(context) {
-    
-    var bet;
-
-    // 1) start up chainfeed.org
+    // Use chainfeed.org for OP_RETURN messages
     let chainfeed = new chainbet.MessageFeed();
     await chainfeed.checkConnection();
 
-    // 2) try to sweep up old escrows accounts from failed bets
+    // TODO: Sweep up old escrows accounts from failed bets
     // var escrows = jsonfile.readFileSync('./examples/escrows.json');
-    // escrows = await chainbet.CoinFlipShared.processOldEscrows(escrows);
+    // escrows = await chainbet.contextShared.processOldEscrows(escrows);
 
-    // 3) startup a single bet workflow
-    if (context.selection == 'host') {
-        // Host Mode
+    // Startup a single bet workflow
+    var bet;
+    if (context.selection == 'host')
         bet = new chainbet.CoinFlipHost(context.wif, context.pubkey, context.address, chainfeed);
-        bet.run();
-    } else { 
-        // Client Mode
-        bet = new chainbet.CoinFlipClient(context.wif, context.pubkey, context.address, chainfeed, coinflip.debug);
-        bet.run();
-    }  
+    else if (context.selection == 'client')
+        bet = new chainbet.CoinFlipClient(context.wif, context.pubkey, context.address, chainfeed, context.debug);
+    bet.run();
 
-    // 4) create while loop to keep program alive while the async bet workflow is running.
+    // Create while loop to keep program alive while the asyncronous bet workflow is running.
     while (!bet.complete) {
         // do other stuff here...
         await chainbet.Utils.sleep(500);
     }
 
-    console.log("\ncoinflip program complete.")
+    console.log("\nContext program complete.");
     process.exit();
+}
+
+async function promptMainMenu() {
+
+    console.log('\n-------------------------------------------------------------------------------');
+    console.log("|                        Welcome to Satoshi's Dice!                           |");
+    console.log('-------------------------------------------------------------------------------');
+    console.log('    =====                   .-------.    ______                       =====');
+    console.log('     ///                   /   o   /|   /\\     \\                       ///');
+    console.log('     ///                  /_______/o|  /o \\  o  \\                      ///');
+    console.log('     ///                  | o     | | /   o\\_____\\                     ///');
+    console.log('     ///                  |   o   |o/ \\o   /o    /                     ///');
+    console.log('     ///                  |     o |/   \\ o/  o  /                      ///');
+    console.log("    =====                 '-------'     \\/____o/                      =====");
+    console.log('-------------------------------------------------------------------------------');
+    console.log("|                      P2P Gaming with Bitcoin Cash                           |");
+    console.log('-------------------------------------------------------------------------------\n');
+    
+    return await inquirer.prompt([{
+            type: "list",
+            name: "selection",
+            message: "What do you want to do?",
+            choices: [new inquirer.Separator("Games"),
+            { name: '  Roll Dice (ODD wins) - You are Host', value: 'host' },
+            { name: '  Roll Dice (EVEN wins) - You wait for a Host', value: 'client' },
+            new inquirer.Separator("Tools"),
+            { name: '  New BCH address', value: 'generate' },
+            { name: '  Withdraw all BCH', value: 'withdraw' }
+            ],
+        }]);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -145,10 +120,10 @@ async function debug(context){
     var wallet = jsonfile.readFileSync('./examples/wallet.json');
     var wif; 
 
-    // Host mode debug (i.e., with args "-d 1 -r 1")
+    // Host mode debug (i.e., with context "-d 1 -r 1")
     if(context.selection == "host")
         wif = wallet[0].wif;
-    // Host mode debug (i.e., with args "-d 1 -r 1")
+    // Host mode debug (i.e., with context "-d 1 -r 1")
     else if(context.selection == "client")
         wif = wallet[1].wif;
 
