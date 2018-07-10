1b1166de3864ed7e3692ddfc78c7ae8f422c8e19
James Cramer <jcramer@deskviz.com>
2018-07-10 14:59:51 -0400

    cleanup

--- src/chainbet.ts
+++ src/chainbet.ts
@@ -1,9 +1,12 @@
-export { CoinFlipHost } from './coinfliphost';
-export { CoinFlipClient } from './coinflipclient';
 export { Core } from './core';
+export { Utils } from './utils';
+export { Wallet } from './wallet';
+
 export { Host } from './host';
 export { Client }  from './client';
-export { Utils } from './utils';
-export { MessageFeed} from './messagefeed';
+
+export { CoinFlipHost } from './coinfliphost';
+export { CoinFlipClient } from './coinflipclient';
 export { CoinFlipShared } from './coinflipshared';
-export { Wallet } from './wallet';
+
+export { MessageFeed} from './messagefeed';
